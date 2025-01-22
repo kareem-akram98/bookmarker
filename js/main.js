8921fdd6935ab57080ad87e0992865a314c79a08
kareem-akram98 <kareemakram98@gmail.com>
2025-01-22 20:29:21 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -1,41 +1,45 @@
 
-var siteName =document.getElementById("siteName")
-var siteURL =document.getElementById("siteURL")
+var entryNumber =document.getElementById("entryNumber")
+var projectCode =document.getElementById("projectCode")
+var totalCost =document.getElementById("totalCost")
+var poNumber =document.getElementById("poNumber")
 var alertBox = document.getElementById('boxMsg')
 var closeBtn = document.getElementById('closeBtn')
 
-var linkList =[]
+var project =[]
 
 if (localStorage.getItem('linkContainer') !== null) {
-    linkList = JSON.parse(localStorage.getItem('linkContainer')) 
+    project = JSON.parse(localStorage.getItem('linkContainer')) 
     displayData()
 }
 
 function addBtn(){
     var link = {
-        name : siteName.value ,
-        linkSaved : siteURL.value ,
+        Entry : entryNumber.value ,
+        projectCode : projectCode.value ,
+        cost : totalCost.value ,
+        po : poNumber.value ,
     }
 
-    if (nameRegex.test(link.name)&&urlRegex.test(link.linkSaved))
-    {
-        linkList.push(link)
-        localStorage.setItem('linkContainer', JSON.stringify(linkList))
+    // if (nameRegex.test(link.name)&&urlRegex.test(link.linkSaved))
+    // {
+        project.push(link)
+        localStorage.setItem('linkContainer', JSON.stringify(project))
         displayData()
         clearData()
-        // console.log(linkList)
+        // console.log(project)
         console.log('hi');
         
-    } else {
-        alertBox.classList.remove('d-none')
-        console.log('bye');
-    }
+    // } else {
+    //     alertBox.classList.remove('d-none')
+    //     console.log('bye');
+    // }
 }
 
 
 function visitBtn(indexItem) {
-    console.log(linkList[indexItem].linkSaved);
-    window.open(linkList[indexItem].linkSaved);
+    var webSite = project[indexItem].linkSaved
+    window.open(`https://${webSite}`);
 }
 
 
@@ -47,13 +51,15 @@ function clearData() {
 
 function displayData() {
     var cartona =''
-    for ( i = 0; i < linkList.length; i++) {
-        // console.log(linkList[i].name);
+    for ( i = 0; i < project.length; i++) {
+        // console.log(project[i].name);
         cartona+= `
         <tr>
         <td>${i+1}</td>
-        <td>${linkList[i].name}</td>
-        <td><button onclick='visitBtn(${i})' class="btn btn-visit"><i class="fa-solid fa-eye pe-2"></i>Visit</button></td>
+        <td>${project[i].Entry}</td>
+        <td>${project[i].projectCode}</td>
+        <td>${project[i].cost}</td>
+        <td>${project[i].po}</td>
         <td><button onclick='deletaLink(${i})' class="btn btn-delete"><i class="fa-solid fa-trash-can pe-2"></i>Delete</button></td>
          </tr>        `
     } 
@@ -63,10 +69,10 @@ function displayData() {
 
 function deletaLink(indexItem) {
     // console.log(indexItem);
-    linkList.splice(indexItem , 1)
-    localStorage.setItem('linkContainer', JSON.stringify(linkList))
+    project.splice(indexItem , 1)
+    localStorage.setItem('linkContainer', JSON.stringify(project))
     displayData()
-    // console.log(linkList);
+    // console.log(project);
 }
 
 
@@ -75,18 +81,19 @@ function closeAlert() {
 }
 
 
-const urlRegex = /(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})(\.[a-zA-Z0-9]{2,})?/;
-const nameRegex = /^[a-zA-Z]{3,}$/;
+// const urlRegex = /(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})(\.[a-zA-Z0-9]{2,})?/;
+// const nameRegex = /^[a-zA-Z]{3,}$/;
 
 
-function typing(input,regx) {
-    if (regx.test(input.value))
-        {
-            input.classList.add('is-valid')
-            input.classList.remove('is-invalid')
-        } else {
+// function typing(input,regx) {
+//     if (regx.test(input.value))
+//         {
+//             input.classList.add('is-valid')
+//             input.classList.remove('is-invalid')
             
-            input.classList.add('is-invalid')
-            input.classList.remove('is-valid')
-        }
-}
+//         } else {
+            
+//             input.classList.add('is-invalid')
+//             input.classList.remove('is-valid')
+//                     }
+// }
